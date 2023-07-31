30caa507902eec95ae4db9793646f2e042aa69c7
Shahansha12 <139757163+Shahansha12@users.noreply.github.com>
2023-07-31 16:49:18 +0530

    Add files via upload

---
+++ task7/task7.js
@@ -0,0 +1,43 @@
+import React,{ Component } from 'react';    
+import ReactDOM from 'react-dom';
+class App extends React.Component {  
+   constructor(props) {  
+      super(props);  
+      this.state = {start: "Initial State"};  
+      this.changeState = this.changeState.bind(this)  
+   }    
+   render() {  
+      return (  
+         <div>  
+             <h1>ReactJS component's Lifecycle</h1>  
+             <h3>Hello {this.state.start}</h3>  
+             <button onClick = {this.changeState}>Click Here!</button>          
+         </div>  
+      );  
+   }  
+   componentWillMount() {  
+      console.log('Component Will MOUNT!')  
+   }  
+   componentDidMount() {  
+      console.log('Component Did MOUNT!')  
+   }  
+   changeState(){  
+      this.setState({start:"All!!- Its a great reactjs tutorial."});  
+   }  
+   componentWillReceiveProps(newProps) {      
+      console.log('Component Will Recieve Props!')  
+   }  
+   shouldComponentUpdate(newProps, newState) {  
+      return true;  
+   }  
+   componentWillUpdate(nextProps, nextState) {  
+      console.log('Component Will UPDATE!');  
+   }  
+   componentDidUpdate(prevProps, prevState) {  
+      console.log('Component Did UPDATE!')  
+   }  
+   componentWillUnmount() {  
+      console.log('Component Will UNMOUNT!')  
+   }  
+}  
+ReactDOM.render(<App/>,document.getElementById('root'));
